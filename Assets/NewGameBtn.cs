6a8b6a89194090d59443e90bfdc0d180bb2bea3d
Matthew Alex <doson2435@gmail.com>
2025-11-25 04:38:20 +0700

    Main

---
+++ Assets/NewGameBtn.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NewGameBtn : MonoBehaviour
+{
+
+    public Button btn_gameplay;
+
+    void Start()
+    {
+        btn_gameplay.onClick.AddListener(OnPlayGameButtonClicked);
+    }
+    private void OnPlayGameButtonClicked()
+    {
+        SceneManager.LoadScene("Level 0");
+    }
+}
